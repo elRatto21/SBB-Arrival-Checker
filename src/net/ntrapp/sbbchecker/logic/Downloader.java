fc9fa4169bbc653ce676be5ad1051b949ecb4441
Niklas Trapp <102829294+elRatto21@users.noreply.github.com>
2023-01-19 13:59:22 +0100

    Finished Downloader
    Downloader class for downloading the dataset

--- src/net/ntrapp/sbbchecker/logic/Downloader.java
+++ src/net/ntrapp/sbbchecker/logic/Downloader.java
@@ -4,29 +4,64 @@ import java.io.BufferedInputStream;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.net.MalformedURLException;
 import java.net.URL;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoUnit;
+
+import javax.swing.SwingUtilities;
+
+import net.ntrapp.sbbchecker.gui.LoadingScreen;
 
 public class Downloader {
-	
-	private File tempFile = new File("./dataset/ist-daten-sbb.csv");
+
 	private BufferedInputStream in;
-	
-	
-	public void download() throws IOException {
-		if(!tempFile.exists()) {
-			System.out.println("Download gestartet");
+	private static final DateTimeFormatter DATE_FORMAT = DateTimeFormatter.ofPattern("dd.MM.yyy");
+	// Yesterdays date formatted as dd.MM.yyyy
+	private static final String DATE = LocalDate.now().minus(1, ChronoUnit.DAYS).format(DATE_FORMAT);
+	private File tempFile = new File(String.format("./dataset/%s.csv", DATE));
+
+	// TODO: Implement method to find the real size, currently to lazy so just
+	// assuming the dataset is never bigger than 20MB
+	private static final int DOWNLOAD_SIZE = 20000;
+
+	public Downloader() throws MalformedURLException, IOException {
+		// Checks if the dataset from yesterday already exists
+		if (!tempFile.exists()) {
+			// Prints out that the download starts
+			System.out.println(String.format("Downloading Dataset from %s", DATE));
+			// URL of the dataset (Same URL every day)
 			final String FILE_URL = "https://data.sbb.ch/api/explore/v2.1/catalog/datasets/ist-daten-sbb/exports/csv?lang=de&timezone=Europe%2FBerlin&use_labels=true&csv_separator=%3B";
+			// New InputStream
 			in = new BufferedInputStream(new URL(FILE_URL).openStream());
-			FileOutputStream fileOutputStream = new FileOutputStream("./dataset/ist-daten-sbb.csv");
+			// New OutputStream saving the file as yesterdays date
+			FileOutputStream fileOutputStream = new FileOutputStream(String.format("./dataset/%s.csv", DATE));
+
 			byte dataBuffer[] = new byte[1024];
 			int bytesRead;
-			while((bytesRead = in.read(dataBuffer, 0, 1024)) != -1) {
+			int tempCount = 0; 
+			while ((bytesRead = in.read(dataBuffer, 0, 1024)) != -1) {
+				tempCount++;
+				final int byteCount = tempCount;
+				// Updates the ProgressBar
+				SwingUtilities.invokeLater(new Runnable() {
+					@Override
+					public void run() {
+						if (byteCount % 1000 == 0) {
+							LoadingScreen.updateProgress(byteCount);
+						}
+					}
+				});
+				// "Downloading"
 				fileOutputStream.write(dataBuffer, 0, bytesRead);
 			}
-			System.out.println("Download beendet");
+			// Download finished, updating ProgressBar to 100%
+			System.out.println("Download finished");
+			LoadingScreen.updateProgress(DOWNLOAD_SIZE);
 			fileOutputStream.close();
 		} else {
-			System.out.println("Datei bereits vorhanden");
+			System.out.println("Dataset already exists");
 		}
 	}
 
