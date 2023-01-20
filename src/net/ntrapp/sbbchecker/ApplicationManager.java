e2ff014c58ca39501b93b6da44a3aa4f7852f921
Niklas Trapp <102829294+elRatto21@users.noreply.github.com>
2023-01-20 23:56:26 +0100

    Small changes

--- src/net/ntrapp/sbbchecker/ApplicationManager.java
+++ src/net/ntrapp/sbbchecker/ApplicationManager.java
@@ -1,18 +1,13 @@
-package net.ntrapp.sbbchecker;
+package net.ntrapp.sbbchecker; 
 
-import java.io.FileNotFoundException;
 import java.io.IOException;
 
 import net.ntrapp.sbbchecker.gui.Menu;
-import net.ntrapp.sbbchecker.logic.Downloader;
-import net.ntrapp.sbbchecker.logic.Parser;
 
 public class ApplicationManager {
 
 	public static void main(String[] args) throws IOException {
-		//Menu menu = Menu.getInstance();
-		Downloader d = new Downloader();
-		d.download();
+		Menu.getInstance();
 	}
 
 }
